620e559e6dc7cd099186973a5b09004ecfdbfc43
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-06 16:30:37 +0800

    add state 组件

--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,20 @@
-import React from 'react'; 
+import React from 'react';
 
 class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: new Date()
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <h2>{this.state.date.toString()}</h2>
+            </div>
+        )
+    }
 }
 
 export default Welcome 
