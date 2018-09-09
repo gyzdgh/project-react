bf82d25e381618ac5c86d0e98e160cf6273e5c0e
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-09-09 13:41:10 +0800

    完成基本骨架

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Welcome from './welcome'
 
 
-  
-  ReactDOM.render(
-    <Welcome name="ggg"/>,
-    document.getElementById('root')
-  )
-  
+
+// ReactDOM.render(
+//     <Welcome name="ggg" />,
+//     document.getElementById('root')
+// )
